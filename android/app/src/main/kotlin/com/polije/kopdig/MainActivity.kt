6adff8ba7216996fa0a0db5b8e9e38925d3b9a0d
ibnu009 <ibnubatutah001@gmail.com>
2022-04-21 19:34:02 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/polije/kopdig/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.polije.kopdig
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/theme/kopdig_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class KopdigTheme {
+  static const Color primaryColor = Color(0xFF95AE36);
+  static const Color primaryColorDarker = Color(0xFF5B7207);
+
+}
