e1cc0f778a327679c85d5ba80b56e5458b4e505b
Daffa Alfin Nurilanbiya <daffaalfin2905@gmail.com>
2022-05-30 18:41:04 +0700

    keranjang

--- lib/ui/theme/kopdig_theme.dart
+++ lib/ui/theme/kopdig_theme.dart
@@ -3,43 +3,58 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const String poppins = 'Poppins';
+double defaultMargin = 30.0;
 
 class KopdigTheme {
   static const Color primaryColor = Color(0xFF95AE36);
   static const Color primaryColorDarker = Color(0xFF5B7207);
   static const Color greydark = Color(0xFF504F5E);
+  static const Color alert = Color(0xffED6363);
+
+  static TextStyle get primaryTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: Colors.black,
+      );
+
+  static TextStyle get priceTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: KopdigTheme.primaryColor,
+      );
+
+  static TextStyle get alertTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: KopdigTheme.alert,
+      );
 
   static TextStyle get title => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.primaryColorDarker,
-    fontWeight: FontWeight.w600,
-    fontSize: 24,
-  );
+        poppins,
+        color: KopdigTheme.primaryColorDarker,
+        fontWeight: FontWeight.w600,
+        fontSize: 24,
+      );
 
-  static TextStyle get text => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.greydark,
-    fontWeight: FontWeight.w400,
-    fontSize: 16
-  );
+  static TextStyle get text => GoogleFonts.getFont(poppins,
+      color: KopdigTheme.greydark, fontWeight: FontWeight.w400, fontSize: 16);
 
   static TextStyle get title1 => GoogleFonts.getFont(
-    poppins,
-    color: Colors.black,
-    fontWeight: FontWeight.w600,
-    fontSize: 16,
-  );
+        poppins,
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+      );
 
-  static TextStyle get text1 => GoogleFonts.getFont(
-      poppins,
-      color: Colors.black,
-      fontWeight: FontWeight.w400,
-      fontSize: 14
-  );
+  static TextStyle get text1 => GoogleFonts.getFont(poppins,
+      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 14);
   static TextStyle get darktitle => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.greydark,
-    fontWeight: FontWeight.w600,
-    fontSize: 24,
-  );
+        poppins,
+        color: KopdigTheme.greydark,
+        fontWeight: FontWeight.w600,
+        fontSize: 24,
+      );
 }
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
