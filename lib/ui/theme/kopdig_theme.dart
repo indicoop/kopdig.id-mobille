002496b874b427129c3841a598d0a4aab23f5df9
Daffa Alfin Nurilanbiya <daffaalfin2905@gmail.com>
2022-05-30 18:41:04 +0700

    keranjang

--- lib/ui/theme/kopdig_theme.dart
+++ lib/ui/theme/kopdig_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const String poppins = 'Poppins';
+double defaultMargin = 30.0;
 
 class KopdigTheme {
 
@@ -11,38 +12,52 @@ class KopdigTheme {
   static const Color primaryColorDarker = Color(0xFF5B7207);
   static const Color greydark = Color(0xFF504F5E);
   static const Color fieldColor = Color(0xFFF3F2F2);
+  static const Color alert = Color(0xffED6363);
+
+  static TextStyle get primaryTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: Colors.black,
+      );
+
+  static TextStyle get priceTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: KopdigTheme.primaryColor,
+      );
+
+  static TextStyle get alertTextStyle => GoogleFonts.getFont(
+        poppins,
+        color: KopdigTheme.alert,
+      );
 
   static TextStyle get title => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.primaryColorDarker,
-    fontWeight: FontWeight.w600,
-    fontSize: 24,
-  );
+        poppins,
+        color: KopdigTheme.primaryColorDarker,
+        fontWeight: FontWeight.w600,
+        fontSize: 24,
+      );
 
-  static TextStyle get text => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.greydark,
-    fontWeight: FontWeight.w400,
-    fontSize: 16
-  );
+  static TextStyle get text => GoogleFonts.getFont(poppins,
+      color: KopdigTheme.greydark, fontWeight: FontWeight.w400, fontSize: 16);
 
   static TextStyle get title1 => GoogleFonts.getFont(
-    poppins,
-    color: Colors.black,
-    fontWeight: FontWeight.w600,
-    fontSize: 16,
-  );
+        poppins,
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+      );
 
-  static TextStyle get text1 => GoogleFonts.getFont(
-      poppins,
-      color: Colors.black,
-      fontWeight: FontWeight.w400,
-      fontSize: 14
-  );
+  static TextStyle get text1 => GoogleFonts.getFont(poppins,
+      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 14);
   static TextStyle get darktitle => GoogleFonts.getFont(
-    poppins,
-    color: KopdigTheme.greydark,
-    fontWeight: FontWeight.w600,
-    fontSize: 24,
-  );
+        poppins,
+        color: KopdigTheme.greydark,
+        fontWeight: FontWeight.w600,
+        fontSize: 24,
+      );
 }
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
---
+++ lib/widgets/keranjang_card.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:kopdig/ui/theme/kopdig_theme.dart';
+
+class KeranjangCard extends StatelessWidget {
+  // final KeranjangModel cart; //keranjang model API
+  KeranjangCard(this.cart);
+
+  @override
+  Widget build(BuildContext context) {
+    // Butuh API
+    // CartProvider cartProvider = Provider.of<CartProvider>(context);
+
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      cart.product.galleries[0].url,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      cart.product.name,
+                      style: KopdigTheme.primaryTextStyle.copyWith(
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      '\$${cart.product.price}',
+                      style: KopdigTheme.priceTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      cartProvider.addQuantity(cart.id); // bingung API
+                    },
+                    child: Image.asset(
+                      'assets/button_add.png',
+                      width: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    cart.quantity.toString(), // bingung API
+                    style: KopdigTheme.primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      cartProvider.reduceQuantity(cart.id); // bingung API
+                    },
+                    child: Image.asset(
+                      'assets/button_min.png',
+                      width: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          GestureDetector(
+            onTap: () {
+              cartProvider.removeCart(cart.id); // bingung API
+            },
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/icon_remove.png',
+                  width: 10,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'Remove',
+                  style: KopdigTheme.alertTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: light,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
